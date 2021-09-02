7dbfdd1d7d82f4c6c6932463b04343c068c1d144
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2021-09-01 20:53:09 -0400

    refactored routers

---
+++ src/routers/user.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const User = require("../models/user");
+const router = express.Router();
+
+router.post("/users", async (req, res) => {
+  const user = new User.User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  const _id = req.params.id;
+  try {
+    const user = await User.User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const user = await User.User.findByIdAndDelete(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
